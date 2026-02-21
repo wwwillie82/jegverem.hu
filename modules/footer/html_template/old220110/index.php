09d4caec7b5873accb963686033aa30cfb200e81
wwwillie82 <167377112+wwwillie82@users.noreply.github.com>
2026-02-21 23:21:21 +0100

    Footer polish: legal link behavior and legacy banner cleanup

--- modules/footer/html_template/old220110/index.php
+++ modules/footer/html_template/old220110/index.php
@@ -1,24 +1,14 @@
-<div id="partners">
-	<a href="http://civitashotel.hu" class="icon_civitas" target="_blank">Civitas Hotel</a>
-	<a href="http://soproniborhaz.hu" class="icon_borhaz" target="_blank">Soproni Borház</a>
-	<a href="http://voltszallas.hu" class="icon_volt" target="_blank">Voltszállás</a>
-	<a href="http://jegverem.hu" class="icon_jegverem" target="_blank">Jégverem Fogadó</a>
-	<a href="http://palatinussopron.com" class="icon_palatinus" target="_blank">Palatinus Szálló</a>
-	<a href="http://palatinusgroup.com" class="icon_group" target="_blank">Palatinus Group</a>
-</div>
-
-<div id="footer">
-    <div class="links">
-        <a href="/index" <? if($_SERVER["REQUEST_URI"] == "/index" || $_SERVER["REQUEST_URI"] == "/"): ?>class="on"<? endif; ?>>Kezdőlap</a> <span>&bull;</span>
-        <a href="/heti_menu" <? if($_SERVER["REQUEST_URI"] == "/heti_menu"): ?>class="on"<? endif; ?>>Heti menü</a> <span>&bull;</span>
-        <a href="/ettermunk_kinalata" <? if($_SERVER["REQUEST_URI"] == "/ettermunk_kinalata"): ?>class="on"<? endif; ?>>Éttermünk kínálata</a> <span>&bull;</span>
-        <a href="/galeria" <? if($_SERVER["REQUEST_URI"] == "/galeria"): ?>class="on"<? endif; ?>>Galéria</a> <span>&bull;</span>
-        <a href="/a_panziorol" <? if($_SERVER["REQUEST_URI"] == "/a_panziorol"): ?>class="on"<? endif; ?>>A panzióról</a> <span>&bull;</span>
-        <a href="/a_jegverem_tortenete" <? if($_SERVER["REQUEST_URI"] == "/a_jegverem_tortenete"): ?>class="on"<? endif; ?>>A Jégverem története</a> <span>&bull;</span>
-        <a href="/kapcsolat" <? if($_SERVER["REQUEST_URI"] == "/kapcsolat"): ?>class="on"<? endif; ?>>Kapcsolat</a>
+<div id="footer" class="footer-simple">
+    <div class="footer-partners" aria-label="Partnereink">
+        <img src="/images/skin_v2/hotel_civitas_sopron_logo.png" alt="Hotel Civitas Sopron" loading="lazy" />
+        <img src="/images/skin_v2/palatinus_logo_2025.png" alt="Palatinus" loading="lazy" />
+        <img src="/images/skin_v2/sopronfest_szallas.png" alt="SopronFest Szállás" loading="lazy" />
     </div>
 
-    <div class="copyright">
-        Copyright Jégverem Fogadó Sopron. Minden jog fenntartva. Site by <a href="http://voov.hu">Voov</a>.
+    <div class="footer-legal">
+        <span class="footer-copy">© 2021 Palatinus Group. Minden jog fenntartva.</span>
+        <a class="footer-legal-link" href="#" onclick="return false;">Jogi nyilatkozat</a>
+        <a class="footer-legal-link" href="#" onclick="return false;">Adatkezelési tájékoztató</a>
     </div>
 </div>
+<script src="/js/header_sticky.js" type="text/javascript"></script>
