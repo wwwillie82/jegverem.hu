f4fa58dd56aa8dbbb6062a2357fc0cffeb001e90
wwwillie82 <167377112+wwwillie82@users.noreply.github.com>
2026-03-07 13:46:44 +0100

    Fix homepage controller file to use full PHP opening tag

--- apps/index.php
+++ apps/index.php
@@ -1,11 +1,11 @@
-<? 
+<?php
 	class Index extends Controller {
 		function main() {
 			$this->view->header = Loader::Factory("header/index")->Execute();
 			$this->view->banner = Loader::Factory("banner/index")->Execute();
 			$this->view->footer = Loader::Factory("footer/index")->Execute();
 			
-			// aktuális ajánlat 119
+			// aktuÃ¡lis ajÃ¡nlat 119
 			$current_date = date("Y-m-d");
 			$category = $this->db->Query("SELECT * FROM categories WHERE parent_id='119' AND from_date<='$current_date' AND to_date>='$current_date' LIMIT 0,1");
 			$this->view->category = $category;
